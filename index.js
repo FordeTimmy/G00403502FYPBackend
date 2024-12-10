80648850a2ba16f71da149ef0fdb827dcc952ead
Timmy Forde <you@example.com>
2024-12-10 16:59:16 +0000

    Trying to add json web tokens

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -8,6 +10,27 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 app.use(express.json());
+
+// Middleware to verify JWT
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+
+// Login route - generates token
+app.post('/api/login', (req, res) => {
+  const user = { id: 1, username: req.body.username };
+  const accessToken = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
+  res.json({ accessToken });
+});
 
 // Test route to see if server is working
 app.get('/api/test', (req, res) => {
